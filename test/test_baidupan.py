b46dace379b9b822ace467d3321450d082383551
Low Kian Seong <kianseong.low@onapp.com>
2015-09-21 02:06:42 +0800

    Added another long test for _request method

--- test/test_baidupan.py
+++ test/test_baidupan.py
@@ -58,6 +58,77 @@ class DefaultTestCase(unittest.TestCase):
                               from_path='from_path',
                               to='to_path'))
 
+    @patch("requests.post")
+    @patch("requests.get")
+    def test_private_request(self, mock_get, mock_post):
+        # Disable the earlier patch on _request to properly test it.
+        self.request_patcher.stop()
+        baidupan = BaiduPan()
+        baidupan.base_url = 'base_url'
+        baidupan.urlpath = 'urlpath'
+        baidupan.method = 'method'
+        baidupan.access_token = 'access_token'
+        baidupan._method = "GET"
+        mock_get.return_value.content = 'hulahoop'
+        result = baidupan._request(headers='headers',
+                                   file='file1',
+                                   filename='file1',
+                                   from_path='from_path',
+                                   content_length='content_length',
+                                   content_md5='content_md5',
+                                   slice_md5='slice_md5',
+                                   content_crc32='content_crc32')
+        self.assertTrue(mock_get.call_args, call('base_urlurlpath', headers='headers',
+                                                 params={'slice-md5': 'slice_md5',
+                                                         'content-length': 'content_length',
+                                                         'from': 'from_path',
+                                                         'access_token': 'access_token',
+                                                         'filename': 'file1',
+                                                         'content-crc32': 'content_crc32',
+                                                         'content-md5': 'content_md5',
+                                                         'method': 'method'}))
+        self.assertEqual(result, 'hulahoop')
+        mock_post.return_value.content = 'hulahoop'
+        baidupan._method = "POST"
+        result_post = baidupan._request(headers='headers',
+                                        file='file1',
+                                        filename='file1',
+                                        from_path='from_path',
+                                        content_length='content_length',
+                                        content_md5='content_md5',
+                                        slice_md5='slice_md5',
+                                        content_crc32='content_crc32')
+        self.assertEqual(mock_post.call_args, call('base_urlurlpath', files={'files': ('file1', 'file1')},
+                                                   headers='headers',
+                                                   params={'slice-md5': 'slice_md5',
+                                                           'content-length': 'content_length',
+                                                           'from': 'from_path',
+                                                           'access_token': 'access_token',
+                                                           'filename': 'file1',
+                                                           'content-crc32': 'content_crc32',
+                                                           'content-md5': 'content_md5',
+                                                           'method': 'method'}))
+        self.assertEqual(result_post, 'hulahoop')
+        baidupan.payload = 'payload'
+        baidupan.files = None
+        mock_post.reset_mock()
+        result_payload = baidupan._request(headers='headers')
+        self.assertEqual(mock_post.call_args, call('base_urlurlpath', data='payload',
+                                                   headers='headers',
+                                                   params={'slice-md5': 'slice_md5',
+                                                           'content-length': 'content_length',
+                                                           'from': 'from_path',
+                                                           'access_token': 'access_token',
+                                                           'filename': 'file1',
+                                                           'content-crc32': 'content_crc32',
+                                                           'content-md5': 'content_md5',
+                                                           'method': 'method'}))
+        self.assertEqual(result_payload, 'hulahoop')
+        baidupan._method = 'not allowed'
+        with self.assertRaises(Exception):
+            baidupan._request(headers="headers")
+        self.request_patcher.start()
+
 
 def suite():
     suite = unittest.TestSuite()
